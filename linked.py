e0607fd13741895b4bf4e8cc8cea473b539a9152
joedegere <degerej@hartwick.edu>
2020-03-05 08:43:29 -0500

    File updated!

--- linked.py
+++ linked.py
@@ -5,14 +5,5 @@ class Stack:
     def __init__(self):
         self.my_list = LinkedList()
 
-    def push_head(self, data):
+    def push(self, data):
         self.my_list.add_head(data)
-
-    def push_end(self, data):
-        self.my_list.add_end(data)
-
-    def pop_head(self):
-        self.my_list.remove_head()
-
-    def pop_end(self):
-        self.my_list.remove_end()
